199ea90d2b1b73bb110db25ecdf72009d44bb723
Fabricio Nogueira <fabriciopinheiro713@gmail.com>
2021-05-26 14:04:04 -0300

    refactor: refactoring controllers and variable names.

--- atividade-1/app/controllers/users.js
+++ atividade-1/app/controllers/users.js
@@ -1,81 +1,83 @@
 // ARRAY USERS
-let usuarios = [
-  { id: 2, nome: "Fabrício", email: "fabricio@gmail.com", senha: 111 },
-  { id: 3, nome: "Larissa", email: "larissa@gmail.com", senha: 222 },
-  { id: 4, nome: "Lionel", email: "lionel@gmail.com", senha: 333 },
+let Users = [
+  { id: 2, nome: "Lionel Messi", email: "messi@gmail.com", senha: 10 },
+  { id: 3, nome: "Cristiano Ronaldo", email: "ronaldo@gmail.com", senha: 07 },
+  { id: 4, nome: "Neymar Júnior", email: "neymar@gmail.com", senha: 11 },
 ];
+
 // ARRAY POSTS
 let posts = [
   { id: 5, texto: "Tudo bom! E vc?", likes: 6 },
-  { id: 2, texto: "Hoje é Quarta", likes: 8 },
+  { id: 2, texto: "Hoje o dia foi divertido", likes: 8 },
   { id: 3, texto: "Corinthians Maior Campeão Paulista", likes: 7 },
 ];
 
 // CONTROLLERS USERS
 
 // Inserir Usuários
-module.exports.inserirUsuarios = function (req, res) {
-  let usuario = req.body;
-  usuarios.push(usuario);
-  res.status(201).json(usuario);
+module.exports.insertUsers = (req, res) => {
+  let user = req.body;
+  Users.push(user);
+  res.status(201).json(user);
 };
 
 // Listar Usuários
-module.exports.listarUsuarios = function (req, res) {
-  let retorno_usuarios = usuarios;
-  if (retorno_usuarios.length > 0) res.json(retorno_usuarios);
-  else res.status(404).json({ mensagem: "Lista de usuários vazia" });
+module.exports.listUsers = (req, res) => {
+  let return_users = Users;
+  if (return_users.length > 0) res.json(return_users);
+  else res.status(404).json({ message: "Lista de usuários vazia" });
 };
 
 // Buscar Usuário por ID
-module.exports.buscarUsuarioPorId = function (req, res) {
+module.exports.searchUserById = (req, res) => {
   let id = req.params.id;
-  let usuario = usuarios.find((usuario) => {
-    return usuario.id == id;
+  let user = Users.find((user) => {
+    return user.id == id;
   });
-  if (usuario) res.json(usuario);
-  else res.status(404).json({ mensagem: "Usuario não encontrado" });
+  if (user) res.json(user);
+  else res.status(404).json({ message: "Usuario não encontrado" });
 };
 
 // Deletar Usuário por ID
-module.exports.removerUsuarioPorId = (req, res) => {
+module.exports.removeUserById = (req, res) => {
   let id = req.params.id;
-  usuarios = usuarios.filter((usuario) => {
-    return usuario.id != id;
+  Users = Users.filter((user) => {
+    return user.id != id;
   });
-  res.json({ mensagem: "Usuário removido" });
+  res.json({ message: "Usuário removido" });
 };
 
 // CONTROLLERS POSTS
 
 // Inserir Posts
-module.exports.inserirPosts = (req, res) => {
+module.exports.insertPosts = (req, res) => {
   let post = req.body;
   posts.push(post);
   res.status(201).json(post);
 };
 
 // Listar Posts
-module.exports.listarPosts = (req, res) => {
+module.exports.listPosts = (req, res) => {
   let retorno_posts = posts;
   if (retorno_posts.length > 0) res.json(retorno_posts);
-  else res.status(404).json({ mensagem: "Lista de posts vazia" });
+  else res.status(404).json({ message: "Lista de posts vazia" });
 };
 
 // Buscar Post por ID
-module.exports.buscarPostById = (req, res) => {
+module.exports.searchPostById = (req, res) => {
   let id = req.params.id;
   let post = posts.find((post) => {
     return post.id == id;
   });
   if (post) res.json(post);
-  else res.status(404).json({ mensagem: "Post não encontrado" });
+  else res.status(404).json({ message: "Post não encontrado" });
 };
 
-module.exports.deletarPostById = (req, res) => {
+// Deletar Post por ID
+module.exports.removePostById = (req, res) => {
   let id = req.params.id;
   posts = posts.filter((post) => {
     return post.id != id;
   });
-  res.json({ mensagem: "Post removido" });
+  res.json({ message: "Post removido" });
 };
--- atividade-1/app/routes/users.js
+++ atividade-1/app/routes/users.js
@@ -4,28 +4,28 @@ module.exports = (app) => {
   // ENDPOINT USERS
 
   // POST - Inserir usuário
-  app.post("/usuarios", controller.inserirUsuarios);
+  app.post("/usuarios", controller.insertUsers);
 
   // GET - Listar usuários
-  app.get("/usuarios", controller.listarUsuarios);
+  app.get("/usuarios", controller.listUsers);
 
   // GET - Retorna usuário com um dado ID
-  app.get("/usuarios/:id", controller.buscarUsuarioPorId);
+  app.get("/usuarios/:id", controller.searchUserById);
 
   // DELETE - Remove usuário com id dado
-  app.delete("/usuarios/:id", controller.removerUsuarioPorId);
+  app.delete("/usuarios/:id", controller.removeUserById);
 
   // ENDPOINT POSTS
 
   // POST - Inserir Posts
-  app.post("/posts", controller.inserirPosts);
+  app.post("/posts", controller.insertPosts);
 
   // GET - Listar Posts
-  app.get("/posts", controller.listarPosts);
+  app.get("/posts", controller.listPosts);
 
   // GET - Retorna Post com um dado ID
-  app.get("/posts/:id", controller.buscarPostById);
+  app.get("/posts/:id", controller.searchPostById);
 
   // DELETE - Remove Post
-  app.delete("/posts/:id", controller.deletarPostById);
+  app.delete("/posts/:id", controller.removePostById);
 };
